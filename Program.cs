95fc7e1c58d002c096ba5f5826abce162c5615c0
ahnjh97 <ahnjh1997@naver.com>
2026-08-26 08:35:01 +0900

    feat: 다익스트라 기반 최단 경로 탐색 구현

--- Program.cs
+++ Program.cs
@@ -3,7 +3,27 @@
 subway.AddStation("강남", "2호선");
 subway.AddStation("강남", "신분당선");
 
+subway.ConnectStations("강남", "2호선", "강남", "신분당선", 180);
+
 var stations = subway.FindStations("강남");
 
 foreach (Station station in stations)
+{
     Console.WriteLine(station.Name + " " + station.Line);
+    foreach(var kvp in station.Neighbors)
+    {
+        Station neighbor = kvp.Key;
+        Console.WriteLine("이웃: " + neighbor.Name + " " + neighbor.Line + ", 소요시간: " + kvp.Value);
+    }
+    Console.WriteLine();
+}
+
+try
+{
+    subway.FindShortestPath("교대", "교대");
+}
+catch(ArgumentException e)
+{
+    Console.WriteLine("오류: " + e.Message);
+}
+    
--- Subway.cs
+++ Subway.cs
@@ -42,8 +42,101 @@ public class Subway
         return FindStations(name).FirstOrDefault(station => station.Line == line);
     }
 
+    // 최단시간 및 경로 찾기
     public (List<Station> Path, int TotalTime) FindShortestPath(string startName, string endName)
     {
-        throw new NotImplementedException();
+        List<Station> startStations = FindStations(startName);
+        List<Station> endStations = FindStations(endName);
+
+        // 예외처리
+        const String errorStr = "존재하지 않는 역입니다.";
+
+        if (startStations.Count == 0 && endStations.Count == 0)
+            throw new ArgumentException("출발역과 도착역이 " + errorStr);
+        else if(startStations.Count == 0)
+            throw new ArgumentException("출발역이 " + errorStr);
+        else if(endStations.Count == 0)
+            throw new ArgumentException("도착역이 " + errorStr);
+
+        if (startName == endName)
+            throw new ArgumentException("출발역과 도착역이 동일합니다.");
+
+        // 다익스트라
+        Dictionary<Station, int> distances = new();
+        Dictionary<Station, Station> previous = new();
+
+        PriorityQueue<Station, int> pq = new();
+
+        foreach (List<Station> stations in stations.Values)
+        {
+            foreach(Station station in stations)
+            {
+                distances[station] = int.MaxValue;
+                previous[station] = null;
+            }
+        }
+
+        // 충정로 2호선, 충정로 5호선 처럼 모든 출발역 후보를 시작점으로 추가
+        foreach (Station src in startStations)
+        {
+            distances[src] = 0;
+            pq.Enqueue(src, 0);
+        }
+
+        Station dst = null;
+
+        while (pq.Count > 0)
+        {
+            pq.TryDequeue(out Station cur, out int curDist);
+
+            if (cur == null)
+                continue;
+
+            if (curDist > distances[cur])
+                continue;
+
+            // 도착역 후보 중 하나에 도착했다면 종료
+            if (endStations.Contains(cur))
+            {
+                dst = cur;
+                break;
+            }
+
+            foreach (var neighbor in cur.Neighbors)
+            {
+                Station next = neighbor.Key;
+                int travelTime = neighbor.Value;
+
+                int newDist = curDist + travelTime;
+
+                if (newDist < distances[next])
+                {
+                    distances[next] = newDist;
+                    previous[next] = cur;
+
+                    pq.Enqueue(next, newDist);
+                }
+            }
+        }
+
+        // 도착역까지 갈 수 없는 경우
+        if (dst == null)
+            throw new InvalidOperationException("도착역까지 이동할 수 없습니다.");
+
+        // 최단 경로 복원
+        List<Station> path = new();
+
+        Station curStation = dst;
+
+        while (curStation != null)
+        {
+            path.Add(curStation);
+            curStation = previous[curStation];
+        }
+
+        // 도착 -> 출발 순서로 들어갔으므로 뒤집기
+        path.Reverse();
+
+        return (path, distances[dst]);
     }
 }
